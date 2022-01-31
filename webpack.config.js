0bf3ebfe295b1f9f05f022c68ce1d124acb70eb0
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-01-31 22:00:52 +0000

    chore: output to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './main.js',
   output: {
-    path: __dirname,
+    path: `${__dirname}/dist`,
     filename: 'out.js'
   },
   experiments: {asyncWebAssembly: true},
@@ -17,5 +18,10 @@ module.exports = {
       stream: 'readable-stream/readable.js',
       crypto: false
     }
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
 }

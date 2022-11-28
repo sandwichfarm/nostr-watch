20e31671d1a1d37a1eb729aef18644f98ecf5e15
dskvr <dskvr@users.noreply.github.com>
2022-11-28 22:57:52 +0100

    add leaflet and component

---
+++ src/components/LeafletComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+
+    <l-marker v-for="([relay, result]) in Object.entries(results)" :lat-lng="result.geo.latlng" draggable>
+      <l-popup>
+        {{ result.uri }}
+      </l-popup>
+    </l-marker>
+  </l-map>
+</template>
+<script>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+  },
+  data() {
+    return {
+      markers: {
+        type: Object,
+        default(){
+          return {}
+        }
+      }
+    };
+  },
+};
+</script>
+
+<style></style>

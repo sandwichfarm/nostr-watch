7d4699fce0579d3b320f89fe3e8c26e4e0b4f767
dskvr <dskvr@users.noreply.github.com>
2022-12-20 02:21:11 +0100

    add like component

---
+++ src/components/LikeComponent.vue
@@ -0,0 +1,23 @@
+<script>
+console.log(window.nostr)
+// func make_like_event(pubkey, privkey: String, liked: NostrEvent) => {
+//     { 
+//         content: "+", 
+//         pubkey: pubkey, 
+//         kind: 7, 
+//         tags: tags 
+//     }
+
+// }
+//     NostrEvent {
+//     var tags: [[String]] = liked.tags.filter { 
+//     	tag in tag.count >= 2 && (tag[0] == "e" || tag[0] == "p") 
+//     }
+//     tags.append(["e", liked.id])
+//     tags.append(["p", liked.pubkey])
+//     let ev = NostrEvent(content: "+", pubkey: pubkey, kind: 7, tags: tags)
+//     ev.calculate_id()
+//     ev.sign(privkey: privkey)
+//     return ev
+// }
+</script>

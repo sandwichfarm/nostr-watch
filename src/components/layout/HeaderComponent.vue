c7ddb4133d40676e681c9105dbd01c835af990d7
dskvr <dskvr@users.noreply.github.com>
2023-01-12 15:29:54 +0100

    fix subsection-header bg

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -45,6 +45,7 @@
         </div>
 
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          
           <DarkMode />
           
           <!-- Profile dropdown -->

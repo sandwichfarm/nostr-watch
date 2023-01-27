1e06ae775c3fafebbfd29062b20a16dc91f8bc4e
dskvr <dskvr@users.noreply.github.com>
2023-01-27 15:23:07 +0100

    hotfix: latency label text

--- src/components/relays/pages/RelaysSingle.vue
+++ src/components/relays/pages/RelaysSingle.vue
@@ -86,7 +86,7 @@
             <span>{{ result.latency.final }}</span>
           </div> -->
           <div class="text-black dark:text-white text-lg md:text-xl lg:text-3xl flex-1 block py-6">
-            <h3 class="text-white/50 text-lg">Avg. Latency</h3>
+            <h3 class="text-black/70 dark:text-white/50 text-lg">Avg. Latency</h3>
             <svg v-if="!result.latency.average" class="animate-spin mr-1 mt-1 h-6 w-6 text-black dark:text-white inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -94,7 +94,7 @@
             <span>{{ result.latency.average }}</span>
           </div>
           <div class="text-black dark:text-white text-lg md:text-xl lg:text-3xl flex-1 block py-6">
-            <h3 class="text-white/50 text-lg">Min Latency</h3>
+            <h3 class="text-black/70 dark:text-white/50 text-lg">Min Latency</h3>
             <svg v-if="!result.latency.min" class="animate-spin mr-1 mt-1 h-6 w-6 text-black dark:text-white inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -102,7 +102,7 @@
             <span>{{ result.latency.min }}</span>
           </div>
           <div class="text-black dark:text-white text-lg md:text-xl lg:text-3xl flex-1 block py-6">
-            <h3 class="text-white/50 text-lg">Max Latency</h3>
+            <h3 class="text-black/70 dark:text-white/50 text-lg">Max Latency</h3>
             <svg v-if="!result.latency.max" class="animate-spin mr-1 mt-1 h-6 w-6 text-black dark:text-white inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>

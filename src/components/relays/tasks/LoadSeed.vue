81294131fa4a5864837452841f6c901ac4545f44
dskvr <dskvr@users.noreply.github.com>
2023-02-04 16:57:14 +0100

    fix nip-11 detection with clientSideProcessing off

--- src/components/relays/tasks/LoadSeed.vue
+++ src/components/relays/tasks/LoadSeed.vue
@@ -25,6 +25,8 @@ import { relays } from '../../../../relays.yaml'
 import { RelayPool } from 'nostr'
 import { geo } from '../../../../cache/geo.yaml'
 
+import { Inspector } from 'nostr-relay-inspector'
+
 const localMethods = {
   invalidate(force, single){
     // if( ( this.store.tasks.getLastUpdate('relays/check') || ( this.store.tasks.processed?.['relays/check'] && this.store.tasks.processed?.['relays/check'].length ) ) && !force ) 
@@ -34,51 +36,100 @@ const localMethods = {
     
     this.queueJob(
       this.slug, 
-      () => {
-        const pool = new RelayPool(['wss://history.nostr.watch'])
-        const subid = crypto.randomBytes(40).toString('hex')
-        pool
-          .on('open', relay => {
-            console.log('subscribing')
-            relay.subscribe(subid, {
-              kinds:    [30303],
-              "#d":     single ? [ single ] : relays,
-              authors:  ['b3b0d247f66bf40c4c9f4ce721abfe1fd3b7529fbc1ea5e64d5f0f8df3a4b6e6'],
-            })
-          })
-          .on('event', (relay, sub_id, event) => {
-            if(subid === sub_id){
-              const relay = event.tags[0][1]
-              const data = JSON.parse(event.content)
-              const result = {
-                url: relay,
-                check: {
-                  connect: data.connect,
-                  read: data.read,
-                  write: data.write,
-                },
-                latency: data?.latency[this.store.prefs.region],
-                info: data.info,
-                uptime: this.getUptimePercentage(relay),
-                identities: []
-              }
-              
-              if(data.info?.pubkey)
-                result.identities.push(data.info?.pubkey)
+      async () => {
+        const relayChunks = this.chunk(100, [...relays])
+        console.log(relayChunks)
+        const promises = []
+        for (let i = 0; i < relayChunks.length; i++) {
+          const promise = await new Promise( resolve => {
+            const relayChunk = relayChunks[i]
+            const pool = new RelayPool(['wss://history.nostr.watch'])
+            const subid = `${crypto.randomBytes(40).toString('hex')}-${i}`
+            pool
+              .on('open', relay => {
+                console.log('subscribing', relayChunk)
+                relay.subscribe(subid, {
+                  kinds:    [30303],
+                  "#d":     single ? [ single ] : relayChunk,
+                  authors:  ['b3b0d247f66bf40c4c9f4ce721abfe1fd3b7529fbc1ea5e64d5f0f8df3a4b6e6'],
+                })
+              })
+              .on('event', async (relay, sub_id, event) => {
+                if(subid === sub_id){
+                  console.log('event', event.tags)
+                  const relay = event.tags[0][1]
+                  const data = JSON.parse(event.content)
+                  const result = {
+                    url: relay,
+                    latency: data?.latency[this.store.prefs.region],
+                    info: data.info,
+                    uptime: this.getUptimePercentage(relay),
+                    check: {
+                      connect: false,
+                      read: false,
+                      write: false
+                    },
+                    identities: []
+                  }
 
-              if(data?.topics)
-                result.topics = data.topics.filter( topic => !this.store.prefs.ignoreTopics.split(',').includes(topic[0]) )
+                  if(event?.tags){
+                    const connect = event.tags.filter( tag => tag[0] == 'c'),
+                          read = event.tags.filter( tag => tag[0] == 'r'),
+                          write = event.tags.filter( tag => tag[0] == 'w')
+                    result.check = {
+                      connect: connect.length && connect[0][1] === 'true' ? true : false,
+                      read: read.length && read[0][1] ? true : false,
+                      write: write.length && write[0][1] ? true : false,
+                    }
+                  }
+                    
+
+                  console.log('result', result)
+
+                  if(result?.check?.connect) {
+                    console.log('inspecting', result.url)
+                    const inspector = new Inspector(relay, {
+                      checkRead: false,
+                      checkWrite: false,
+                      checkLatency: false,
+                      checkAverageLatency: false,
+                      passiveNipTests: false,
+                      getInfo: true,
+                      run: true
+                    })
+
+                    inspector.on('complete', inspect => {
+                      const res = inspect.result 
+                      result.pubkeyValid = res.pubkeyValid 
+                      result.pubkeyError = res.pubkeyError 
+                      result.identities = res.identities
+                      console.log('post history node data', result.pubkeyValid, result.pubkeyError, result.identities)
+                      this.results[relay] = result
+                      this.setCache(result)
+                    })
+                  } 
                 
-              result.aggregate = this.getAggregate(result)
-              this.results[relay] = result
-              this.setCache(result)
-            }
+                  if(data.info?.pubkey)
+                    result.identities.push(data.info?.pubkey)
+
+                  if(data?.topics)
+                    result.topics = data.topics.filter( topic => !this.store.prefs.ignoreTopics.split(',').includes(topic[0]) )
+                    
+                  result.aggregate = this.getAggregate(result)
+                  this.results[relay] = result
+                  this.setCache(result)
+                }
+              })
+              .on('eose', () => {
+                pool.unsubscribe(subid)
+                pool.close()
+                resolve()
+              })
           })
-          .on('eose', () => {
-            pool.unsubscribe(subid)
-            this.store.tasks.completeJob()
-            pool.close()
-          })
+          promises.push(promise)
+        }
+        await Promise.all(promises)
+        this.store.tasks.completeJob()
       },
       true
     )
@@ -96,6 +147,22 @@ const localMethods = {
   timeSinceRefresh(){
     return this.timeSince(this.store.tasks.getLastUpdate(this.slug)) || Date.now()
   },
+  chunk(chunkSize, array) {
+    return array.reduce(function(previous, current) {
+        var chunk;
+        if (previous.length === 0 || 
+                previous[previous.length -1].length === chunkSize) {
+            chunk = [];
+            previous.push(chunk);
+        }
+        else {
+            chunk = previous[previous.length -1];
+        }
+        chunk.push(current);
+        return previous;
+    }, []); 
+  }
+
 }
 
 export default defineComponent({
@@ -103,7 +170,8 @@ export default defineComponent({
   components: {},
   data() {
     return {
-      slug: 'relays/seed' //REMEMBER TO CHANGE!!!
+      slug: 'relays/seed', //REMEMBER TO CHANGE!!!
+      pool: null
     }
   },
   setup(props){
@@ -118,6 +186,7 @@ export default defineComponent({
   },
   unmounted(){
     clearInterval(this.interval)
+    this.pool = null
   },
   beforeMount(){
     this.lastUpdate = this.store.tasks.getLastUpdate(this.slug)

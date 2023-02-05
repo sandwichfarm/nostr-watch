309d59a4887713c7499e1dfd548ba3de72fa1ba7
dskvr <dskvr@users.noreply.github.com>
2023-02-05 02:09:07 +0100

    add topics to relay list

--- src/components/relays/tasks/GetTopics.vue
+++ src/components/relays/tasks/GetTopics.vue
@@ -2,20 +2,9 @@
   <span 
     v-if="this.store.tasks.getActiveSlug === slug"
     class="text-white lg:text-sm mr-2 ml-2 mt-1.5 text-xs">
-    <span v-if="isSingle">Loading {{ relay }} from history node...</span>
-    <span v-if="!isSingle">Loading data from history relay</span>
-  </span>
-  <span 
-    v-if="!store.tasks.isActive || this.store.tasks.getActiveSlug === slug">
-    <span class="text-white lg:text-sm mr-2 ml-2 text-xs mt-1.5 inline-block mr-10" v-if="store.prefs.refresh && !store.tasks.isProcessing(this.slug)"> 
-      Next check in: {{ untilNext }}
-    </span>
+    <span v-if="!isSingle">Loading trending topics...</span>
   </span>
 </template>
-
-<style scoped>
-
-</style>
 
 <script>
 import { defineComponent, toRefs } from 'vue'
@@ -29,9 +18,6 @@ import SharedComputed from '@/shared/computed.js'
 import { relays } from '../../../../relays.yaml'
 
 import { RelayPool } from 'nostr'
-import { geo } from '../../../../cache/geo.yaml'
-
-import { Inspector } from 'nostr-relay-inspector'
 
 const localMethods = {
   invalidate(force, single){
@@ -53,7 +39,6 @@ const localMethods = {
             const subid = `${crypto.randomBytes(40).toString('hex')}-${i}`
             pool
               .on('open', relay => {
-                console.log('subscribing', relayChunk)
                 relay.subscribe(subid, {
                   kinds:    [30303],
                   "#d":     single ? [ single ] : relayChunk,
@@ -65,65 +50,10 @@ const localMethods = {
                   console.log('event', event.tags)
                   const relay = event.tags[0][1]
                   const data = JSON.parse(event.content)
-                  const result = {
-                    url: relay,
-                    latency: data?.latency[this.store.prefs.region],
-                    info: data.info,
-                    uptime: this.getUptimePercentage(relay),
-                    check: {
-                      connect: false,
-                      read: false,
-                      write: false,
-                      latency: data?.latency[this.store.prefs.region]?.final ? true : false,
-                      averageLatency: data?.latency[this.store.prefs.region]?.average ? true : false
-                    },
-                    identities: []
-                  }
-
-                  if(event?.tags){
-                    const connect = event.tags.filter( tag => tag[0] == 'c'),
-                          read = event.tags.filter( tag => tag[0] == 'r'),
-                          write = event.tags.filter( tag => tag[0] == 'w')
-                    result.check = {
-                      connect: connect.length && connect[0][1] === 'true' ? true : false,
-                      read: read.length && read[0][1] === 'true' ? true : false,
-                      write: write.length && write[0][1] === 'true' ? true : false,
-                    }
-                  }
                   
-                  console.log('result', result)
-
-                  if(result?.check?.connect) {
-                    console.log('inspecting', result.url)
-                    const inspector = new Inspector(relay, {
-                      checkRead: false,
-                      checkWrite: false,
-                      checkLatency: false,
-                      checkAverageLatency: false,
-                      passiveNipTests: false,
-                      getInfo: true,
-                      run: true
-                    })
-
-                    inspector.on('complete', inspect => {
-                      const res = inspect.result 
-                      result.pubkeyValid = res.pubkeyValid 
-                      result.pubkeyError = res.pubkeyError 
-                      result.identities = res.identities
-                      console.log('post history node data', result.pubkeyValid, result.pubkeyError, result.identities)
-                      this.results[relay] = result
-                      this.setCache(result)
-                    })
-                  } 
-                
-                  if(data.info?.pubkey)
-                    result.identities.push(data.info?.pubkey)
-
                   if(data?.topics)
-                    result.topics = data.topics.filter( topic => !this.store.prefs.ignoreTopics.split(',').includes(topic[0]) )
-                    
-                  result.aggregate = this.getAggregate(result)
-                  this.results[relay] = result
+                    this.results[relay].topics = data.topics.filter( topic => !this.store.prefs.ignoreTopics.split(',').includes(topic[0]) )
+
                   this.setCache(result)
                 }
               })
@@ -144,12 +74,9 @@ const localMethods = {
   setRefreshInterval: function(){
     clearInterval(this.interval)
     this.interval = setInterval(() => {
-      this.lastUpdate = this.store.tasks.getLastUpdate(this.slug)
-      this.untilNext = this.timeUntilRefresh()
-      this.sinceLast = this.timeSinceRefresh()
       if(!this.store.tasks.isProcessing(this.slug) && !this.isSingle)
         this.invalidate()
-    }, 15*60*1000)
+    }, 60*60*1000)
   },
   timeUntilRefresh(){
     return this.timeSince(Date.now()-(this.store.tasks.getLastUpdate(this.slug)+this.refreshEvery-Date.now())) 
@@ -201,18 +128,7 @@ export default defineComponent({
     this.pool = null
   },
   beforeMount(){
-    this.lastUpdate = this.store.tasks.getLastUpdate(this.slug)
-    this.untilNext = this.timeUntilRefresh()
-    this.sinceLast = this.timeSinceRefresh()
-    
-    this.store.relays.setGeo(geo)
     this.relays = Array.from(new Set(relays))
-
-    for(let ri=0;ri-this.relays.length;ri++){
-      const relay = this.relays[ri],
-            cache = this.getCache(relay)
-      this.results[relay] = cache
-    }
   },
   mounted(){
     console.log('is processing', this.store.tasks.isProcessing(this.slug))
@@ -247,11 +163,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style scoped>
- #refresh { font-size: 12pt; color:#666; margin-bottom:15px }
- #refresh button { cursor: pointer; border-radius: 3px; border: 1px solid #a0a0a0; color:#333 }
- #refresh button:hover {color:#000;}
- #refresh button[disabled] {color:#999 !important; border-color:#e0e0e0}
-</style>
-

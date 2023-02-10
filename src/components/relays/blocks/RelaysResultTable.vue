62e5e7fdd64bf7311a6a89b698b39f5af4aa3d9d
dskvr <dskvr@users.noreply.github.com>
2023-02-10 12:29:17 +0100

    add money icon

--- src/components/relays/blocks/RelaysResultTable.vue
+++ src/components/relays/blocks/RelaysResultTable.vue
@@ -122,7 +122,7 @@
                       </td> -->
 
                       <td v-if="!store.layout.editorIsExpanded" class="w-12 verified text-center hidden md:table-cell lg:table-cell xl:table-cell">
-                        {{ isPayToRelay(relay) }}
+                        <a v-if="isPayToRelay(relay)" href="this.results?.[relay]?.info?.payments_url" class="block align-center" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="inline-block align-center w-6 h-6 stroke-green-600/90 dark:stroke-green-400/90"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg></a>
                       </td>
 
                       <td v-if="!store.layout.editorIsExpanded && (store.prefs.checkNip11 || subsection === 'nips')" class="w-12 verified text-center hidden md:table-cell lg:table-cell xl:table-cell">
@@ -402,7 +402,7 @@
       isPayToRelay(){
         return relay => {
           if(this.results?.[relay]?.info?.payments_url)
-            return "$"
+            return true
         }
         
       },

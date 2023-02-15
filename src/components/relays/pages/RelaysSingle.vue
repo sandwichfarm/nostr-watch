06aff0600b05e1e4b45f2de27f77ce4909399958
dskvr <dskvr@users.noreply.github.com>
2023-02-15 16:46:56 +0100

    fix first load bug

--- src/components/relays/pages/RelaysSingle.vue
+++ src/components/relays/pages/RelaysSingle.vue
@@ -176,7 +176,7 @@
 
 
         <div class="flex-none lg:flex mb-8">
-          <!-- <div class="flex-none lg:flex-1 justify-center mb-6 lg:mb-0"  v-if="geo">
+          <div class="flex-none lg:flex-1 justify-center mb-6 lg:mb-0"  v-if="geo && Object.keys(geo)?.length">
             <div class="inline-block rounded-lg shadow-lg h-auto lg:h-full bg-white dark:bg-black/30 max-w-sm text-center">
               <div class="py-6 px-4">
                 <h5 class="text-gray-900 dark:text-white/90 text-xl font-medium mb-2">
@@ -189,16 +189,16 @@
                 </p>
               </div>
             </div>
-          </div> -->
+          </div>
 
-          <div class="flex-none lg:flex-1 justify-center mb-6 lg:mb-0" v-if="Object.keys(result?.info)?.length">
+          <div class="flex-none lg:flex-1 justify-center mb-6 lg:mb-0" v-if="result?.info && Object.keys(result?.info)?.length">
             <div class="inline-block rounded-lg shadow-lg h-auto lg:h-full bg-white dark:bg-black/30 max-w-sm text-center">
               <div class="py-6 px-4">
                 <h5 class="text-gray-900 dark:text-white/90 text-xl font-medium mb-2">
                   Software
                 </h5>
                 <p class="text-gray-700 text-base mb-4 dark:text-white/60">
-                  <strong>{{relay}}</strong> is running <strong>{{ getSoftware }}</strong> version <strong>{{ result.info.version }}</strong>
+                  <strong>{{relay}}</strong> is running <strong>{{ getSoftware }}</strong> version <strong>{{ result?.info?.version }}</strong>
                 </p>
               </div>
             </div>

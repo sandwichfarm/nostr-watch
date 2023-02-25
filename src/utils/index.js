5ad6f799aefe3a9b2009d8f2707540d61b6b60c7
horberlan <horberlan@gmail.com>
2023-02-25 17:20:53 -0300

    chore: clean up code from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,98 +1,97 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
-//geo
-import { daemons } from '@/config/nwd-geo.yaml'
-import { getDistance } from 'geolib';
-import { continents } from '../../cache/continents.js'
 import { RelayPool } from 'nostr'
+import { continents } from '../../cache/continents.js'
 import crypto from 'crypto'
+import { daemons } from '@/config/nwd-geo.yaml'
+import { getDistance } from 'geolib'
 
-export const timeSince = function(date) {
-  const delta = (Date.now()-parseInt(date))/1000,
-        seconds = Math.floor(delta)
-    
-  let   interval = seconds / 31536000
+export const timeSince = date => {
+  const delta = (Date.now() - date) / 1000
+  const seconds = Math.floor(delta)
 
-  if (interval > 1) {
-    return Math.floor(interval) + " years"
+  if (seconds < 60) {
+    return `${Math.floor(seconds)} seconds`
   }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months"
+
+  const intervals = [
+    {label: 'years', value: 31536000},
+    {label: 'months', value: 2592000},
+    {label: 'days', value: 86400},
+    {label: 'hours', value: 3600},
+    {label: 'minutes', value: 60}
+  ]
+
+  for (const {label, value} of intervals) {
+    const interval = seconds / value
+    if (interval >= 1) {
+      return `${Math.floor(interval)} ${label}`
+    }
   }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days"
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours"
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes"
-  }
-  return Math.floor(seconds) + " seconds"
+
+  return `${Math.floor(seconds)} seconds`
 }
 
-export const getClosest = function(visitorGeo){
-  const distances = []
-  Object.keys(daemons).forEach( region => {
-    //console.log('type', region, daemons, typeof daemons[region].lon, daemons[region].lon)
+export const getClosest = visitorGeo => {
+  const distances = Object.keys(daemons).map(region => {
     const distance = getDistance(
-      { latitude: visitorGeo.lat, longitude: visitorGeo.lon },
-      { latitude: daemons[region].lat, longitude: daemons[region].lon }
+      {latitude: visitorGeo.lat, longitude: visitorGeo.lon},
+      {latitude: daemons[region].lat, longitude: daemons[region].lon}
     )
-    distances.push({ region: region, distance: distance })
+    return {region: region, distance: distance}
   })
-  distances.sort( (a, b) => {
-    return a.distance - b.distance
-  })
+  distances.sort((a, b) => a.distance - b.distance)
   return distances[0].region
 }
 
-export const getGeo = async function(relay){
+export const getGeo = async function (relay) {
   let dns, ip, geo
-  //console.log('ip-api key', process.env.VUE_APP_IP_API_KEY)
-  dns = await getDnsFromRelay(relay).catch()
-  ip = await getIPFromDNS(dns).catch()
-  geo = await getGeoFromIP(ip).catch()
-
-  if(geo)
-    geo = Object.assign(geo, getContinentFromCountryCode(geo.countryCode))
-  if(geo && dns)
-    geo.dns = dns[dns.length-1]
-  if(geo && geo.status == 'success') {
+  try {
+    dns = await getDnsFromRelay(relay)
+    ip = await getIPFromDNS(dns)
+    geo = await getGeoFromIP(ip)
+  } catch (err) {
+    console.warn(`no geo result for: ${relay}`)
+    return
+  }
+  if (geo) {
+    geo = {
+      ...geo,
+      ...getContinentFromCountryCode(geo.countryCode)
+    }
+  }
+  if (geo && dns) {
+    geo.dns = dns[dns.length - 1]
+  }
+  if (geo && geo.status === 'success') {
     delete geo.status
   }
-  if(!geo)
-    console.warn(`no geo result for: ${relay}`)
-
   return geo
 }
 
-export const getVisitorGeo = async function() {
+export const getVisitorGeo = async function () {
   //console.log('ip-api key', process.env.VUE_APP_IP_API_KEY)
   let geo
-  const url = process.env.VUE_APP_IP_API_KEY ? `https://pro.ip-api.com/json/?fields=ip,lat,lon&key=${process.env.VUE_APP_IP_API_KEY}` : `http://ip-api.com/json/?fields=ip,lat,lon`
-  await fetch(url, { headers: { 'accept': 'application/dns-json' } })
-          .then(response => response.json())
-          .then((data) => { geo = data })
-          .catch(err => console.error('./scripts/geo.js', err))
-  return geo;
+  const url = process.env.VUE_APP_IP_API_KEY
+    ? `https://pro.ip-api.com/json/?fields=ip,lat,lon&key=${process.env.VUE_APP_IP_API_KEY}`
+    : `http://ip-api.com/json/?fields=ip,lat,lon`
+  await fetch(url, {headers: {accept: 'application/dns-json'}})
+    .then(response => response.json())
+    .then(data => {
+      geo = data
+    })
+    .catch(err => console.error('./scripts/geo.js', err))
+  return geo
 }
 
-export const getIPFromDNS = async function(dns){
-  let ip;
-  if(dns)
-    ip = dns[dns.length-1].data
+export const getIPFromDNS = async function (dns) {
+  let ip
+  if (dns) ip = dns[dns.length - 1].data
   return ip
 }
 
-export const getContinentFromCountryCode = function(countryCode) {
+export const getContinentFromCountryCode = function (countryCode) {
   return continents
-    .filter( c => c.country_code == countryCode )
-    .map( cont => {
+    .filter(c => c.country_code == countryCode)
+    .map(cont => {
       return {
         continentCode: cont.continent_code,
         continentName: cont.continent_name
@@ -100,78 +99,73 @@ export const getContinentFromCountryCode = function(countryCode) {
     })[0]
 }
 
-export const getGeoFromIP = async function(ip){
+export const getGeoFromIP = async function (ip) {
   const query = ip
   let geo
-  const url = process.env.VUE_APP_IP_API_KEY ? `https://pro.ip-api.com/json/${query}?key=${process.env.VUE_APP_IP_API_KEY}` : `http://ip-api.com/json/`
-  await fetch(url, { headers: { 'accept': 'application/dns-json' } })
-          .then(response => response.json())
-          .then((data) => { geo = data })
-          .catch(err => console.error('./scripts/geo.js', err))
-  return geo;
+  const url = process.env.VUE_APP_IP_API_KEY
+    ? `https://pro.ip-api.com/json/${query}?key=${process.env.VUE_APP_IP_API_KEY}`
+    : `http://ip-api.com/json/`
+  await fetch(url, {headers: {accept: 'application/dns-json'}})
+    .then(response => response.json())
+    .then(data => {
+      geo = data
+    })
+    .catch(err => console.error('./scripts/geo.js', err))
+  return geo
 }
 
-export const getDnsFromRelay = async function(relay){
+export const getDnsFromRelay = async function (relay) {
   const query = new URL(relay).hostname
   let dns
-  await fetch(`https://1.1.1.1/dns-query?name=${query}`, { headers: { 'accept': 'application/dns-json' } })
+  await fetch(`https://1.1.1.1/dns-query?name=${query}`, {
+    headers: {accept: 'application/dns-json'}
+  })
     .then(response => response.json())
-    .then((data) => { dns = data.Answer ? data.Answer : false })
+    .then(data => {
+      dns = data.Answer ? data.Answer : false
+    })
     .catch(err => console.error('./scripts/geo.js', err))
   return dns
 }
 
-export const subscribeKind3 = async function(pubkey, relays){
-  return new Promise( resolve => {
-    const pool = new RelayPool(relays, { reconnect: true }),
-          subid = crypto.randomBytes(40).toString('hex'),
-          ordered = [],
-          total = relays.length
+export const subscribeKind3 = async function (pubkey, relays) {
+  const pool = new RelayPool(relays, {reconnect: true})
+  const subid = crypto.randomBytes(40).toString('hex')
+  const ordered = []
+  const total = relays.length
+  let eose = 0
 
-    let eose = 0
-
-    const complete = function(){
-      if(!ordered.length)
-        return resolve({})
-      ordered.sort( (a, b) => {
-        return b.created_at - a.created_at
-      })
-      try{pool.close()} catch(e){""}
-      clearTimeout(timeout)
-      resolve(ordered[0])
-    }
-
-    const timeout = setTimeout( () => complete(), 10000 )
+  return new Promise(resolve => {
+    const timeout = setTimeout(() => resolve({}), 10000)
 
     pool
       .on('open', r => {
         r.subscribe(subid, {
           limit: 1,
           kinds: [3],
-          authors:[ pubkey ]
+          authors: [pubkey]
         })
       })
       .on('event', (relay, _subid, ev) => {
-        if(_subid !== subid)
-          return 
-        
-        if(!ev.content.length)
-          return
+        if (_subid !== subid) return
+        if (!ev.content.length) return
         try {
           ev.content = JSON.parse(ev.content)
-        }
-        catch(e){
+        } catch (e) {
           ev.content = {}
         }
         ordered.push(ev)
       })
       .on('eose', () => {
-        // console.log('eose', eose, '/', total, eose < total)
         eose++
-        if(eose < total)
-          return 
-        complete()
+        if (eose < total) return
+        ordered.sort((a, b) => b.created_at - a.created_at)
+        try {
+          pool.close()
+        } catch (e) {}
+        clearTimeout(timeout)
+        resolve(ordered[0])
       })
-      .on('error', () => { })
+      .on('error', () => {})
   })
 }

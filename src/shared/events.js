a6ab591957c308cbf8577a30060166784b77c311
horberlan <horberlan@gmail.com>
2023-02-25 18:34:12 -0300

    Refactor events.js to improve code readability

--- src/shared/events.js
+++ src/shared/events.js
@@ -1,81 +1,79 @@
-import {
-  getEventHash,
-  getPublicKey,
-  signEvent,
-  validateEvent,
-  verifySignature
-} from 'nostr-tools'
+import { getEventHash, getPublicKey, signEvent } from 'nostr-tools'
 
-import {RelayPool} from 'nostr'
-import crypto from 'crypto'
+import { RelayPool } from 'nostr'
+import crypto from 'crypto';
+import { useUserStore } from '@/store/user'
+
+const store = useUserStore()
 
 const events = {}
 
-events.discoverRelays = async function () {
+events.discoverRelays = async function(){
   return new Promise(resolve => {
     const subid = crypto.randomBytes(40).toString('hex')
-    const pool = new RelayPool(['wss://nostr.sandwich.farm'])
+    const pool = RelayPool(['wss://nostr.sandwich.farm'])
     pool
       .on('open', relay => {
-        relay.subscribe(subid, {limit: 1000, kinds: [3]})
+        //console.log('open')
+        relay.subscribe(subid, {limit: 1000, kinds:[3]})
       })
-      .on('close', () => {})
+      .on('close', () => {
+        //console.log('close')
+      })
       .on('event', (relay, _subid, event) => {
-        if (subid === _subid) {
-          try {
-            relaysRemote = Object.assign(
-              relaysRemote,
-              JSON.parse(event.content)
-            )
+        if(subid == _subid) {
+          try { 
+            relaysRemote = Object.assign(relaysRemote, JSON.parse(event.content))
             this.closeRelay(relay)
-          } catch (e) {}
+          } catch(e) {""}
         }
       })
-    setTimeout(() => {
+    setTimeout( () => {
       this.closePool(pool)
-      resolve(true)
-    }, 10 * 1000)
+      resolve(true) 
+    }, 10*1000 )
   })
 }
 
-events.addRelay = async function () {}
+events.addRelay = async function(){
+  
+}
 
-events.signEvent = async function (event) {
-  const timestamp = Math.floor(Date.now() / 1000)
-  const eventHash = getEventHash(event.id)
-  const publicKey = getPublicKey(privateKey)
-  const signature = await signEvent(event, privateKey)
-
-  const signedEvent = {
+events.signEvent = async function(event) {
+  let newEvent = {
     kind: 10101,
-    created_at: timestamp,
-    tags: ['online', 'nostr-watch'],
-    content: 'hello',
-    id: eventHash,
-    pubkey: publicKey,
-    sig: signature
+    created_at: Date.now(),
+    tags: ["online", "nostr-watch"],
+    content: 'hello'
   }
 
-  const isValid = validateEvent(signedEvent)
-  const isVerified = await verifySignature(signedEvent)
+  newEvent.id = getEventHash(event.id);
+  newEvent.pubkey = getPublicKey();
+  newEvent.sig = await signEvent(newEvent, store.getPublicKey);
+
+  // let ok = validateEvent(newEvent);
+  // let veryOk = await verifySignature(newEvent);
 }
 
-events.get = async function () {
-  const pool = new RelayPool(this.relays)
-  const subid = crypto.randomBytes(40).toString('hex')
+events.get = async function (){
+  const pool = new RelayPool(this.relays),
+        subid = crypto.randomBytes(40).toString('hex')
 
   pool
-    .on('open', () => {
-      relay.subscribe(subid, {limit: 10000, kinds: [10101]})
+    .on('open', relay => {
+      relay.subscribe(subid, {limit: 10000, kinds:[10101]})
     })
-    .on('event', () => {})
+    .on('event', () => {
 
-  setTimeout(() => {
+    })
+
+  setTimeout( () => {
     this.closePool(pool)
-    resolve(true)
-  }, 10 * 1000)
+  }, 10*1000 )
 }
 
-events.publish = async function () {}
+events.publish = async function (){
 
-export default Events
+}
+
+export default events;

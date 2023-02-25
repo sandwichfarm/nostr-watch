042dd9c9e29aa9ced6cef070cdfcba1bd5ec921b
horberlan <horberlan@gmail.com>
2023-02-25 17:28:32 -0300

    Update AuthComponent to include additional attributes and improved readability

--- src/components/user/AuthComponent.vue
+++ src/components/user/AuthComponent.vue
@@ -1,53 +1,60 @@
 <template>
-  <a class="text-sm text-white hover:text-white" v-if="signer && !isLoggedIn() && this.store.relays.getFavorites.length && this.store.jobs.isIdle" @click="auth" href="#">Login</a>
+  <a
+    class="text-sm text-white hover:text-white"
+    v-if="
+      signer &&
+      !isLoggedIn() &&
+      this.store.relays.getFavorites.length &&
+      this.store.jobs.isIdle
+    "
+    @click="auth"
+    href="#"
+    >Login</a
+  >
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { useRoute } from 'vue-router'
+import {defineComponent} from 'vue'
+import {useRoute} from 'vue-router'
 import UserLib from '@/shared/user-lib.js'
-import { setupStore } from '@/store'
+import {setupStore} from '@/store'
 import crypto from 'crypto'
-import { RelayPool } from 'nostr'
+import {RelayPool} from 'nostr'
 
 // import { validateEvent, verifySignature, getEventHash } from 'nostr-tools'
 export default defineComponent({
   name: 'AuthComponent',
   components: {},
-  setup(){
-    return { 
-      store : setupStore()
+  setup() {
+    return {
+      store: setupStore()
     }
   },
   data() {
     return {
       route: useRoute(),
-      token : null,
+      token: null,
       user: {},
       signer: false,
       pool: null
     }
   },
-  async mounted(){
+  async mounted() {
     this.showAuth()
-    if(this.isLoggedIn())
-      await this.getData()
+    if (this.isLoggedIn()) await this.getData()
   },
-  updated(){
-  },
+  updated() {},
   computed: {},
   methods: Object.assign(UserLib, {
-    showAuth: async function(){
-      await new Promise( (resolve) => {
-        setTimeout( () => {
-          if(window.nostr instanceof Object)
-            resolve(this.signer = true)
-          else 
-            resolve()
+    showAuth: async function () {
+      await new Promise(resolve => {
+        setTimeout(() => {
+          if (window.nostr instanceof Object) resolve((this.signer = true))
+          else resolve()
         }, 1001)
       })
     },
-    auth: async function(){
+    auth: async function () {
       const pubkey = await window.nostr.getPublicKey()
       this.store.user.setPublicKey(pubkey)
       await this.getData()
@@ -55,41 +62,47 @@ export default defineComponent({
       // this.queueKind3('user/list/contacts')
       this.$forceUpdate()
     },
-    getData: function(){
-      const pool = new RelayPool([...this.store.relays.getFavorites], { reconnect: false })
-      return new Promise( resolve => {
-        const subid = crypto.randomBytes(40).toString('hex')
-        const filterProfile = { limit: 1, kinds:[0], authors: [this.store.user.getPublicKey ] }
-        const filterEvent = { limit: 1, kinds:[1], authors: [this.store.user.getPublicKey ] }
-        let foundProfile = false,
-            foundEvent = false 
-        pool
-          .on('open', Relay => {
-            Relay.subscribe(`${subid}_profile`, filterProfile)
-            Relay.subscribe(`${subid}_event`, filterEvent)
-          })
-        pool 
-          .on('event', (relay, sub_id, event) => {
-            if(`${subid}_profile` == sub_id && !foundProfile) {
-              this.store.user.setProfile(event.content)
-              pool.unsubscribe(sub_id)
-              foundProfile = true
-            }
-            if(`${subid}_event` == sub_id && !foundEvent) {
-              this.store.user.setTestEvent(event)
-              pool.unsubscribe(sub_id)
-              foundEvent = true
-            }
-            if(!foundProfile || !foundEvent)
-              return 
-            resolve()
-          })
+    getData: function () {
+      const pool = new RelayPool([...this.store.relays.getFavorites], {
+        reconnect: false
       })
-    },
+      return new Promise(resolve => {
+        const subid = crypto.randomBytes(40).toString('hex')
+        const filterProfile = {
+          limit: 1,
+          kinds: [0],
+          authors: [this.store.user.getPublicKey]
+        }
+        const filterEvent = {
+          limit: 1,
+          kinds: [1],
+          authors: [this.store.user.getPublicKey]
+        }
+        let foundProfile = false,
+          foundEvent = false
+        pool.on('open', Relay => {
+          Relay.subscribe(`${subid}_profile`, filterProfile)
+          Relay.subscribe(`${subid}_event`, filterEvent)
+        })
+        pool.on('event', (relay, sub_id, event) => {
+          if (`${subid}_profile` == sub_id && !foundProfile) {
+            this.store.user.setProfile(event.content)
+            pool.unsubscribe(sub_id)
+            foundProfile = true
+          }
+          if (`${subid}_event` == sub_id && !foundEvent) {
+            this.store.user.setTestEvent(event)
+            pool.unsubscribe(sub_id)
+            foundEvent = true
+          }
+          if (!foundProfile || !foundEvent) return
+          resolve()
+        })
+      })
+    }
   }),
-  props: {},
+  props: {}
 })
 </script>
 
-<style>
-</style>
+<style></style>

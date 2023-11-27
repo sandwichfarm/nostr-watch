2ca6545617a4dbf7ccb975b44bac4a5cb3e6f0e6
dskvr <dskvr@users.noreply.github.com>
2023-11-27 05:37:12 +0100

    add controlflow package

---
+++ packages/controlflow/index.js
@@ -0,0 +1,11 @@
+import { Trawler, Nocapd, RestApi } from './queues.js'
+import { Scheduler as NWScheduler } from './scheduler.js' 
+
+export default {
+  NWQueues: {
+    Trawler, 
+    Nocapd,
+    RestApi
+  },
+  NWScheduler
+}

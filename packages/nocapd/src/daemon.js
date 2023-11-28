86e572f6d44ad8b66f93aebba7660ab27b3eb4e1
dskvr <dskvr@users.noreply.github.com>
2023-11-29 00:24:55 +0100

    define types

--- packages/nocapd/src/daemon.js
+++ packages/nocapd/src/daemon.js
@@ -1,6 +1,33 @@
-import { NocapdQueue } from '@nostrwatch/controlflow';
+import NocapdWrapper from './NocapWrapper.js'
+import relaydb from '@nostwatch/relaydb'
+import { NocapdQueue, BullQueueEvents, BullWorker } from '@nostrwatch/controlflow'
 
-export startNocapd(){
+const db = new relaydb(process.env.RELAY_DB_PATH || './.lmdb')
 
+const relays = db.relay.all()
+const RelayCheckWebsocket = {
+  url: "",
+  connect: -1,
+  read: -1,
+  write: -1,
+  connectLatency: -1,
+  readLatency: -1,
+  writeLatency: -1,
+  checked_at: -1, 
+  checked_by: ""
 }
 
+const $q = {} 
+      $q.queue = new NocapdQueue()
+
+      $q.worker = new BullWorker($q.queue.name, async job => {
+        const { relay_id, checks } = job.data
+        const relay = db.relay.get(relay_id)
+        const nocapd = new NocapdWrapper(relay)
+        await nocapd.run( checks )
+        return true
+      })
+
+relays.forEach(relay => {
+  
+})

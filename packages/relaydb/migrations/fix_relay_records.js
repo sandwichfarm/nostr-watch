7f34fd8b903cf8838ccf24a16b0474e900a09f42
dskvr <dskvr@users.noreply.github.com>
2023-11-29 00:25:47 +0100

    add relay record migration

---
+++ packages/relaydb/migrations/fix_relay_records.js
@@ -0,0 +1,77 @@
+/**
+ * Fix Note Indices (1):
+ * -----------------------
+ * Some notes were added without the schema being instantiated 
+ * This just reinserts all notes as-is back into the db. 
+ * 
+ * @Additionally, some of the notes had additional fields 
+ * that slipped through the nostr-fetch verifier
+ * remove them.
+ */
+
+
+import Relaydb from '../index.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const dbpath = process.env.RELAYDB_PATH
+
+const relaydb = Relaydb(dbpath? dbpath : './.lmdb')
+
+console.log(relaydb.$.env.stat())
+
+const chunkArray = function(arr, chunkSize) {
+  if (chunkSize <= 0) {
+    throw new Error("Chunk size must be greater than 0.");
+  }
+  const result = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  return result;
+}
+
+  // const relays = await relaydb.relay.get.allIds()
+  console.log(relaydb.relay.count.all())
+  const relays = await relaydb.relay.get.all()
+  
+  const chunks = chunkArray(relays, 100)
+
+  // console.log(chunks)
+  // process.exit()
+  
+
+  let count = 0
+  for(const chunk of chunks){
+    console.log('CHUNKS', chunks.length)
+    for await (const relay of chunk){
+      console.log('SETTING:', relay.url)
+      const url = new URL(relay.url).toString()
+      try {
+        const RELAYRECORD = {
+          url: url,
+          network: relay.network,
+          status: {
+            connect: false,
+            read: false,
+            write: false 
+          },
+          info: "",
+          geo: "",
+          dns: "",
+          ssl: "",
+          checked_at: -1,
+          first_seen: -1,
+          last_seen: -1
+        }
+        console.log(RELAYRECORD)
+        console.log('SET:', `#${count++}`, await relaydb.relay.insert(RELAYRECORD))
+      }
+      catch(e){ console.log('ERROR:', `${relay}: ${e}`) }
+    }
+  }
+
+// }
+
+

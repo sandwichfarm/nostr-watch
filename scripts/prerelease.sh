ae1d6541c6a0618b41a5baaf06580dce44015bd8
dskvr <dskvr@users.noreply.github.com>
2024-09-10 13:34:51 +0200

    Update internal dependencies with actual versions

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -9,13 +9,15 @@ if [[ $1 == "--dry-run" ]]; then
   echo "Running in dry-run mode. No changes will be made."
 fi
 
+# Create an array to store package names and their versions
+package_versions=()
+
 # Get all internal package names and their versions
-declare -A package_versions
 for package_json in $(find . -type f -name "package.json" -not -path "*/node_modules/*"); do
   package_name=$(jq -r '.name // empty' "$package_json")
   package_version=$(jq -r '.version // empty' "$package_json")
   if [[ -n "$package_name" && -n "$package_version" ]]; then
-    package_versions[$package_name]=$package_version
+    package_versions+=("$package_name:$package_version")
   fi
 done
 
@@ -28,22 +30,25 @@ for file in $(find . -type f -name "package.json" -not -path "*/node_modules/*")
     dep=$(echo $dep | tr -d '"')
 
     # Check if the dependency is an internal package
-    if [[ -n "${package_versions[$dep]}" ]]; then
-      dep_version=${package_versions[$dep]}
+    for package in "${package_versions[@]}"; do
+      package_name=${package%%:*}
+      package_version=${package##*:}
 
-      # Provide feedback on what will be replaced
-      echo "Found dependency $dep with version '*'. Will replace it with $dep_version."
+      if [[ "$dep" == "$package_name" ]]; then
+        echo "Found dependency $dep with version '*'. Will replace it with $package_version."
 
-      if [[ $DRY_RUN == false ]]; then
-        # Replace "*" with the actual version in the dependency
-        sed -i '' "s/\"$dep\": \"\*\"/\"$dep\": \"$dep_version\"/g" "$file"
-        echo "Replaced $dep version '*' with $dep_version in $file."
-      else
-        echo "(Dry run) Would replace $dep version '*' with $dep_version in $file."
+        if [[ $DRY_RUN == false ]]; then
+          # Escape special characters in package name
+          escaped_dep=$(echo "$dep" | sed 's/[]\/$*.^[]/\\&/g')
+          # Replace "*" with the actual version in the dependency
+          sed -i '' "s|\"$escaped_dep\": \"\*\"|\"$dep\": \"$package_version\"|g" "$file"
+          echo "Replaced $dep version '*' with $package_version in $file."
+        else
+          echo "(Dry run) Would replace $dep version '*' with $package_version in $file."
+        fi
+        break
       fi
-    else
-      echo "No matching internal dependency found for $dep in $file."
-    fi
+    done
   done
 done
 

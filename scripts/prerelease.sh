7ce98c270a43ee2799fc5546d4bc6c7ec33a133e
dskvr <dskvr@users.noreply.github.com>
2024-09-10 13:30:35 +0200

    Update internal dependencies with actual versions

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -30,14 +30,19 @@ for file in $(find . -type f -name "package.json" -not -path "*/node_modules/*")
     # Check if the dependency is an internal package
     if [[ -n "${package_versions[$dep]}" ]]; then
       dep_version=${package_versions[$dep]}
-      echo "Would update $dep to version $dep_version in $file"
+
+      # Provide feedback on what will be replaced
+      echo "Found dependency $dep with version '*'. Will replace it with $dep_version."
 
       if [[ $DRY_RUN == false ]]; then
         # Replace "*" with the actual version in the dependency
         sed -i '' "s/\"$dep\": \"\*\"/\"$dep\": \"$dep_version\"/g" "$file"
+        echo "Replaced $dep version '*' with $dep_version in $file."
+      else
+        echo "(Dry run) Would replace $dep version '*' with $dep_version in $file."
       fi
     else
-      echo "Error: Could not find version for dependency $dep in $file"
+      echo "No matching internal dependency found for $dep in $file."
     fi
   done
 done

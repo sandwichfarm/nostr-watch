5a399861e9ac5935252bdc426d316bbc70e532fa
dskvr <dskvr@users.noreply.github.com>
2024-09-20 21:26:46 +0200

    fix declaration in src bug

--- libraries/nip66/adapters/cache/DexieAdapter/src/workers/dexie.shared.worker.ts
+++ libraries/nip66/adapters/cache/DexieAdapter/src/workers/dexie.shared.worker.ts
@@ -3,7 +3,7 @@
 import { DexieSharedWorker } from '../DexieSharedWorker';
 
 import { ISharedWorkerGlobalScope } from "@nostrwatch/nip66/interfaces";
-import CacheSharedWorkerFactory from '@nostrwatch/nip66/factory/cacheSharedWorker';
+import CacheSharedWorkerFactory from '@nostrwatch/nip66/cacheSharedWorkerFactory';
 
 const $self = this as unknown as ISharedWorkerGlobalScope;
 
--- libraries/nip66/webpack.config.js
+++ libraries/nip66/webpack.config.js
@@ -4,7 +4,6 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const TerserPlugin = require('terser-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-
 const alias = {
   '@adapters': path.resolve(__dirname, 'adapters'),
   '@core': path.resolve(__dirname, 'src/core'),
@@ -169,82 +168,6 @@ const config = (env) => {
       mode: 'production',
     };
   }
-
-  return [
-    {
-      name: 'browser',
-      target: 'web',
-      entry: './src/index.ts',
-      output: {
-        path: path.resolve(__dirname, 'dist/browser'),
-        filename: 'bundle.js',
-        library: {
-          name: 'NIP66Library',
-          type: 'umd',
-        },
-        clean: true,
-      },
-      resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        alias
-      },
-      module: {
-        rules: [
-          {
-            test: /\.worker\.ts$/,
-            use: [
-              {
-                loader: 'worker-loader',
-                options: {
-                  filename: '[name].js',
-                },
-              },
-              'ts-loader',
-            ],
-          },
-          {
-            test: /\.ts$/,
-            exclude: /\.worker\.ts$/,
-            use: 'ts-loader',
-          },
-        ],
-      },
-      plugins: [],
-      devtool: 'source-map',
-      mode: 'production',
-    },
-    {
-      name: 'server',
-      target: 'node',
-      entry: './src/index.ts',
-      output: {
-        path: path.resolve(__dirname, 'dist/server'),
-        filename: 'bundle.js',
-        library: {
-          type: 'commonjs2',
-        },
-        clean: true,
-      },
-      resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-      },
-      externals: [
-        nodeExternals()
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      plugins: [],
-      devtool: 'source-map',
-      mode: 'production',
-    },
-  ];
 };
 
 module.exports = config;

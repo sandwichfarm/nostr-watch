425bd3c35888466fb6bbc42b024fcc27d0b9e0c1
dskvr <dskvr@users.noreply.github.com>
2024-11-02 19:24:07 +0100

    fix schemata

--- libraries/auditor/src/nips/Nip77/index.ts
+++ libraries/auditor/src/nips/Nip77/index.ts
@@ -1,15 +1,14 @@
 import { SchemaValidator } from "#src/base/SchemaValidator";
 import { INip01Filter, Note } from "../Nip01/interfaces";
-import schemata from './schemata/index.js';
-import type { ClientAuthMessage, RelayAuthMessage } from './interfaces/index.js';
-import { ClientNegOpenMessage } from "./interfaces/ClientNegOpen";
-import { NegCloseMessage } from "./interfaces/NegClose";
-import { NegMsgMessage } from "./interfaces/NegMsg";
-import { NegErrMessage } from "./interfaces/NegErr";
+import schemata from './schemata/index';
 
-type SubscriptionID = string;
-export type ReasonCode = 'RESULTS_TOO_BIG' | 'CLOSED';
-export type HexString = string;
+import type { ClientNegOpenMessage } from "./interfaces/ClientNegOpen.js";
+import type { NegCloseMessage } from "./interfaces/NegClose.js";
+import type { NegMsgMessage } from "./interfaces/NegMsg.js";
+import type { NegErrMessage } from "./interfaces/NegErr.js";
+
+import type { SubscriptionID, HexString } from "./interfaces/GenericTypes.js";
+import type { ReasonCode } from "./interfaces/NegErr.js";
 
 export class Nip77ClientMessageGenerator {
   static NEG_OPEN(subscriptionId: SubscriptionID, filter: INip01Filter, initialMessage: HexString): Buffer {
@@ -43,7 +42,7 @@ export class Nip77 {
   public readonly slug: string = 'Nip01';
 
   readonly messageValidators = {
-    'NEG-OPEN': new SchemaValidator<ClientNegOpenMessage>(schemata.ClientNegOpenMessage), 
+    'NEG-OPEN': new SchemaValidator<ClientNegOpenMessage>(schemata.NegOpenMessage), 
     'NEG-CLOSE': new SchemaValidator<NegCloseMessage>(schemata.NegCloseMessage),
     'NEG-MSG': new SchemaValidator<NegMsgMessage>(schemata.NegMsgMessage),
     'NEG-ERR': new SchemaValidator<NegErrMessage>(schemata.NegErrMessage),

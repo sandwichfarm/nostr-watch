16be6a657bd5e8a250eb0b356c17afb840295d61
Sandwich <299465+dskvr@users.noreply.github.com>
2024-11-25 12:46:12 +0100

    debugging

--- scripts/deploy/gui.sh
+++ scripts/deploy/gui.sh
@@ -1,6 +1,15 @@
 #!/bin/env bash
+echo "BUILDING @nostrwatch/utils"
 yarn workspace @nostrwatch/utils build 
+
+echo "BUILDING @nostrwatch/nip66"
 yarn workspace @nostrwatch/nip66 build
-yarn workspace @nostrwatch/nip66-cacheadapter-nostrsqlite build
+
+echo "BUILDING @nostrwatch/nip66-wsadapter-nostrtools"
 yarn workspace @nostrwatch/nip66-wsadapter-nostrtools build
+
+echo "BUILDING @nostrwatch/nip66-cacheadapter-nostrsqlite"
+yarn workspace @nostrwatch/nip66-cacheadapter-nostrsqlite build
+
+echo "BUILDING @nostrwatch/nip66-cacheadapter-nostrredis"
 yarn workspace @nostrwatch/gui build

289856a3cbc6ff1a1340506f829f39fdfe1c3eaf
Sandwich <299465+dskvr@users.noreply.github.com>
2025-01-24 10:00:29 +0700

    add missing pages

---
+++ apps/gui/src/routes/relays/relay-dimensions.svelte
@@ -0,0 +1,24 @@
+<script>
+
+	import { page } from "$app/stores";
+
+</script>
+
+<div class="flex flex-row ml-3 mt-20 opacity-70">
+    <span class="py-1 px-2 text-sm">Dimension</span>
+    <a href="/relays" class="dimension-link {$page.url.pathname === '/relays'? 'dimension-link-active': ''}">relay</a>
+    <a href="/relays/software" class="dimension-link {$page.url.pathname === '/relays/software'? 'dimension-link-active': ''}">software</a>
+    <a href="/relays/geography" class="dimension-link {$page.url.pathname === '/relays/geography'? 'dimension-link-active': ''}">geo</a>
+    <a href="/relays/isps" class="dimension-link {$page.url.pathname === '/relays/isps'? 'dimension-link-active': ''}">isps</a>
+</div>
+
+<style lang="postcss">
+
+	.dimension-link {
+		@apply py-1 px-2 ml-3 text-sm rounded-sm bg-white/5 hover:bg-white/20;
+	}
+
+    .dimension-link-active {
+        @apply py-1 px-2 ml-3 rounded-sm bg-white/20;
+    }
+</style>
---
+++ apps/gui/src/routes/relays/software/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import { onDestroy, onMount } from 'svelte';
+    import { StateManager } from '@nostrwatch/route66';
+    import { doBootstrap } from '$lib/stores/routines';
+    import { doAggregateCache } from '$lib/stores/app';
+    import { writable, type Writable } from 'svelte/store';
+	import { type DataTableConfig, defaultDataTableConfig } from '$lib/components/lists/table/DataTableTypes';
+    import builtInTableConfig from '$lib/config/dataTable/softwares.js'
+	import { softwareRows as data } from '$lib/stores';
+    import { type default as DataTableType } from '$lib/components/lists/table/DataTable.svelte';
+	import RelayDimensions from '../relay-dimensions.svelte';
+
+    let DataTable: DataTableType;
+    const componentsLoaded: Writable<boolean> = writable(false);
+
+    const loadComponents = async () => {
+		const imports = [
+			import('$lib/components/lists/table/DataTable.svelte'),
+		];
+
+		const results = await Promise.allSettled(imports);
+		[ DataTable ] = results.map(result => (result.status === 'fulfilled' ? result.value.default || result.value : null));
+		componentsLoaded.set(true);
+	}
+
+    const tableKey: string = 'softwares'
+    const config: Writable<DataTableConfig | null> = writable(null);
+    const ready: Writable<boolean> = writable(false);
+
+	const setConfig = () => {
+		
+		let conf = {...defaultDataTableConfig, ...builtInTableConfig}
+
+        //console.log('conf', conf)
+
+		const userTableConfig = StateManager.get(`preferences:${tableKey}:tableConfig`);
+		
+		if(userTableConfig) {
+			conf = {...conf, ...userTableConfig}
+			config.set(conf)
+		}
+		else {
+			config.set(conf)
+		}
+
+        //console.log('config', $config)
+
+		ready.set(true)
+	}
+
+    onMount(async () => {
+        if (typeof window === 'undefined' || typeof navigator === 'undefined') return;
+        doBootstrap.set(true)
+        doAggregateCache.set(true)
+        loadComponents().then(setConfig);
+    });
+
+    onDestroy(() => {
+        ready.set(false)
+    });
+
+</script>
+<!-- <pre>{JSON.stringify($data, null, 4)}</pre> -->
+{#if $ready}
+    <RelayDimensions />
+    <DataTable {data} {config} {tableKey} />
+{/if}

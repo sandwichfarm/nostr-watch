09ace9c289098754bcd858f334d4239de5d2c18a
Sandwich <299465+dskvr@users.noreply.github.com>
2025-01-31 16:09:47 +0700

    fix operators entrypoint bug

--- apps/gui/src/routes/operators/[pubkey]/+page.svelte
+++ apps/gui/src/routes/operators/[pubkey]/+page.svelte
@@ -2,13 +2,27 @@
 	import { page } from "$app/stores";
 	import PageHeader from "$lib/components/layout/PageHeader.svelte";
 	import type { User } from "$lib/models/User";
+	import { dataRegister } from "$stores/data-register";
 	import { pubkeyUserInstance$ } from "$stores/helpers/helpers-pubkey";
 	import { truncateWithEllipsis } from "$utils/strings";
+	import { onMount } from "svelte";
 	import type { Readable } from "svelte/store";
 
     let pubkey = $page.params.pubkey;
     
     const user: Readable<User> = pubkeyUserInstance$(pubkey);
+
+    onMount(async ()=>{
+        await $dataRegister.require(
+            [ 
+                'sync:cache',
+                'sync:relay:operator' 
+            ], 
+            {
+                'sync:relay:operator': [pubkey]
+            }
+        )
+    })
 
     $: name = $user?.name || undefined;
     $: npub = $user?.npub || undefined;
@@ -18,10 +32,16 @@
     $: banner = $user?.banner || undefined;
     $: lud16 = $user?.lud16 || undefined;
     $: lud06 = $user?.lud06 || undefined;
+
+    $: title = name
+        ? name
+        : npub
+            ? truncateWithEllipsis(npub, 21)
+            : truncateWithEllipsis(pubkey, 21)
 </script>
 
 <PageHeader 
-    title={name ?? truncateWithEllipsis(npub, 21) ?? truncateWithEllipsis(pubkey, 21)} 
+    {title} 
     subtitle={about? truncateWithEllipsis(about, 100): undefined} 
     icon={photo}
     {banner}

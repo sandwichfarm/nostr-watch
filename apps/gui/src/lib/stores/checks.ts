8797cbf5bc0ebb189d9d61ceac5b14610a28fed9
Sandwich <299465+dskvr@users.noreply.github.com>
2025-02-09 16:54:03 +0700

    unregister service worker

--- apps/gui/src/lib/stores/checks.ts
+++ apps/gui/src/lib/stores/checks.ts
@@ -134,6 +134,7 @@ export const relayCheckAggregator = ($checks: Nip66CheckEvent[]) => {
         }
         else if(key === 'nip11IsValid'){
           const nip11ValidationErrors = nip11Errors.get(relay)
+          console.log(relay, nip11Errors.get(relay))
           if(typeof nip11ValidationErrors === 'number') {
             acc.nip11IsValid = nip11ValidationErrors === 0? true: false;  
           }

9b287f6b314cb1cd47e5dd73144618be3036dd5a
dskvr <dskvr@users.noreply.github.com>
2025-03-24 16:49:13 +0000

    turn off announce

--- apps/nocapd/src/daemon.js
+++ apps/nocapd/src/daemon.js
@@ -236,6 +236,7 @@ const populateRelays = async ( skipJob = false ) => {
 
     const { Relay } = Schemas
     const syncData = await bootstrap('nocapd').catch( () => log.warn('bootstrap() failed') );
+    console.log('after syncData')
 
     if(!syncData?.[0]) return log.error(`populateRelays(): no relays found in bootstrap data`)
     
@@ -249,6 +250,7 @@ const populateRelays = async ( skipJob = false ) => {
     else {
       await persistRelays({ data: { relays } }).catch(e => log.error(e))
     }
+    console.log('after persistRelays')
     
 }
 
@@ -291,6 +293,9 @@ const globalHandlers = () => {
   
   process.on('unhandledRejection', async (reason, promise) => {
     log.error('!! Unhandled Rejection:', promise.catch(console.error));
+    log.error('UNHANDLED REJECTION:\n', reason?.stack || reason);
+    // Force it to blow up with a real stack trace:
+    throw reason;
   });  
 
   $q.worker.on('error', async (err) => {
@@ -315,7 +320,7 @@ export const Nocapd = async () => {
   concurrency = config?.nocapd?.bullmq?.worker?.concurrency? config.nocapd.bullmq.worker.concurrency: 1
   rcache = relaycache(process.env.NWCACHE_PATH || './.lmdb', lmdbOpts)
   // console.dir(config)
-  await maybeAnnounce();
+  // await maybeAnnounce();
   log.info('Loaded cache...')
   await delay(5000)
   await migrate(rcache)

efcb3ca9d2052e149cd9dbdcd521dc57ac4ae852
sandwich <dskvr@users.noreply.github.com>
2025-10-26 16:06:55 +0100

    fix service

--- libraries/route66/src/services/Service.ts
+++ libraries/route66/src/services/Service.ts
@@ -92,7 +92,7 @@ export class Service {
       let cacheCallbacks: SubscribeHandlers = {}
       if(callbacks) {
         cacheCallbacks = this.addSymbolToCallbacks(callbacks, 'cache');
-        await this.fetchFromCache(filters, cacheCallbacks);
+        this.fetchFromCache(filters, cacheCallbacks);
       }
       else {
         result = this.addSymbolToEvents(await this.fetchFromCache(filters), 'cache');

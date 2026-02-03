1d808bf1583624b23a1e04f5c91aa1d341a446f6
sandwich <299465+dskvr@users.noreply.github.com>
2026-02-03 13:30:06 -0800

    fix(gui): exclude /test-charts from prerender

--- apps/gui/src/routes/test-charts/+page.ts
+++ apps/gui/src/routes/test-charts/+page.ts
@@ -1,8 +1,9 @@
 import { dev } from '$app/environment';
 import { error } from '@sveltejs/kit';
 
+export const prerender = false;
+
 export const load = async () => {
 	if (!dev) throw error(404, 'Not found');
 	return {};
 };
-
